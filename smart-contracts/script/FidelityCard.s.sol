051cab383947e67d0b8d431a44c82ca2c09faae4
TheJoker971 <jojo.blsegor@gmail.com>
2026-05-15 17:40:58 +0200

    Update Solidity version

--- smart-contracts/script/FidelityCard.s.sol
+++ smart-contracts/script/FidelityCard.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.24;
 
 import {Script, console} from "forge-std/Script.sol";
 import {FidelityCard} from "../src/FidelityCard.sol";
--- smart-contracts/src/FidelityCard.sol
+++ smart-contracts/src/FidelityCard.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.24;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
