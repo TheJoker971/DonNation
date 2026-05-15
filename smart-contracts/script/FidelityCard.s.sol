9e19ee45f9f2961e373fea39d87488b99318dbfe
Joackim SEGOR <81244842+TheJoker971@users.noreply.github.com>
2026-05-15 17:05:49 +0200

    Feature/contracts/fidelity card (#2)
    * Create FidelityCard contract
    
    * Adding test of FidelityCard contract (coverage 100%)
    
    * Adding FidelityCard script deployement
    
    * Final commit before review
    
    * Update for metamask
    
    * Format files
    
    * Fix NFT metadata to enable image display in MetaMask
    
    * Adding .gitignore
    
    * Delete smart-contracts/cache directory
    
    * Delete smart-contracts/broadcast directory
    
    * Delete smart-contracts/.env

---
+++ smart-contracts/script/FidelityCard.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {FidelityCard} from "../src/FidelityCard.sol";
+
+contract FidelityCardScript is Script {
+    // metadata of the fidelity card
+    string public anvil =
+        "https://aquamarine-known-rabbit-370.mypinata.cloud/ipfs/bafkreih2rcbqbjgnqqgzelugq67rjdnou26bdopos5kujcqled4jnldzpy";
+
+    function run() public {
+        vm.startBroadcast();
+        FidelityCard fidelityCard = new FidelityCard(msg.sender);
+        fidelityCard.mint(address(msg.sender), 500, anvil);
+        console.log("Fidelity Card minted for 0xAE934c9e17ca46aEb58E88899f14d5bA6e92CC2E");
+        console.log(
+            "Fidelity Card balance of 0xAE934c9e17ca46aEb58E88899f14d5bA6e92CC2E: %s",
+            fidelityCard.balanceOf(msg.sender)
+        );
+        vm.stopBroadcast();
+    }
+}
